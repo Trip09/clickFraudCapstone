c4192034d170bc968cd8a0f40726ef2d3020221d
bbasaran <bbasaran@iupui.edu>
2014-03-30 12:29:43 -0400

    Register Now contains Salt. Tables updated on PHPmyAdmin

--- clientPage/register.php
+++ clientPage/register.php
@@ -9,17 +9,32 @@ if($password1 != $password2){
 }
 if(strlen($username) > 10){
     header('Location: registration.php');
-}   
+}
+
+$hash = hash('sha256', $password1);
+ 
+function createSalt()
+{
+    $text = md5(uniqid(rand(), true));
+    return substr($text, 0, 3);
+}
+ 
+$salt = createSalt();
+$password = hash('sha256', $salt . $hash);
+
 $conn = mysqli_connect('localhost', 'bitnami', 'click_fraud','click_fraud');
 if (mysqli_connect_errno() ){
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
 echo "alive <br>";
+//sanitize username
+$username = mysql_real_escape_string($username);
+
 $password = $password1;
 
-$query = "INSERT INTO member ( username, password, email)
-        VALUES ( '$username', '$password', '$email');";
+$query = "INSERT INTO member ( username, password, email, salt)
+        VALUES ( '$username', '$password', '$email', '$salt');";
 $result = mysqli_query($conn, $query);
 
 echo  "query sucess <br>";
