fe8f79e81728ab69a7d2ac4352cf252b4523eaac
bbasaran <bbasaran@iupui.edu>
2014-03-31 17:30:27 -0400

    Major Change!- Script now Connects to mySQL. Inserts Header Information into Table.
    FreeGeoIP Appears to be down and it was crashing the script. I am working on a fix for that now. The script inserts any non-empty fields into the table. The table is going to need to be updated when I get the remainder of the fields working.

--- test/analysis-test.php
+++ test/analysis-test.php
@@ -11,8 +11,15 @@ if (mysqli_connect_errno() ){
 ////////////////////////////////////////////////////////////////////////////////////////////////
 print <<< HERE
 
-
 HERE;
+
+//function freegeoip_locate($ip){};
+
+//function freegeoip_locate($ip) {
+  //  $url = "http://freegeoip.net/json/".$ip;
+    //$geo = json_decode(file_get_contents($url), true);
+    //return $geo;
+//}
 
 echo "Request information: \n";
 
@@ -57,14 +64,56 @@ foreach ($headers as $name => $value){
 
 }
 
+/*function get_ip_address(){
+    foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key){
+        if (array_key_exists($key, $_SERVER) === true){
+            foreach (explode(',', $_SERVER[$key]) as $ip){
+                $ip = trim($ip); // just to be safe
+
+                if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false){
+                    return $ip;
+                }
+            }
+        }
+    }
+}
+*/
+echo "<br>". $_SERVER['REMOTE_ADDR']. "<br>";
+echo $_SERVER['HTTP_X_FORWARDED_FOR']. "<br>";
+echo "<br>". $_SERVER['GATEWAY_INTERFACE']. "<br>" . $_SERVER['SERVER_ADDR'] . "<br>"
+    . $_SERVER['SERVER_SOFTWARE'] . "<br>" .  $_SERVER['SERVER_PROTOCOL'] . "<br>"
+  . $_SERVER['REQUEST_METHOD'] . "<br>" .  $_SERVER['REQUEST_TIME'] . "<br>"
+  . $_SERVER['REQUEST_TIME_FLOAT'] . "<br>" . $_SERVER['QUERY_STRING'] . "<br>"
+  . $_SERVER['HTTP_ACCEPT_CHARSET'] . "<br>" . $_SERVER['HTTP_ACCEPT_ENCODING'] . "<br>"
+  . $_SERVER['HTTP_ACCEPT_LANGUAGE'] . "<br>" . $_SERVER['HTTP_CONNECTION'] . "<br>"
+  . $_SERVER['HTTP_HOST'] . "<br>" . $_SERVER['HTTP_REFERER'] . "<br>"
+  . $_SERVER['HTTP_USER_AGENT'] . "<br>" . $_SERVER['HTTPS'] . "<br>"
+;
+
+
+$query = "INSERT INTO clicks (
+ REMOTE_ADDR, HTTP_X_FORWARDED_FOR, GATEWAY_INTERFACE, SERVER_ADDR, SERVER_SOFTWARE, SERVER_PROTOCOL ,
+  REQUEST_METHOD, REQUEST_TIME, REQUEST_TIME_FLOAT, QUERY_STRING, HTTP_ACCEPT_CHARSET, HTTP_ACCEPT_ENCODING,
+  HTTP_ACCEPT_LANGUAGE, HTTP_CONNECTION , HTTP_HOST, HTTP_REFERER, HTTP_USER_AGENT , HTTPS
+)VALUES ( '$_SERVER[REMOTE_ADDR]', '$_SERVER[HTTP_X_FORWARDED_FOR]', '$_SERVER[GATEWAY_INTERFACE]' ,'$_SERVER[SERVER_ADDR]' ,
+  '$_SERVER[SERVER_SOFTWARE]', '$_SERVER[SERVER_PROTOCOL]', '$_SERVER[REQUEST_METHOD]' ,'$_SERVER[REQUEST_TIME]' ,
+  '$_SERVER[REQUEST_TIME_FLOAT]' ,'$_SERVER[QUERY_STRING]', '$_SERVER[HTTP_ACCEPT_CHARSET]' ,'$_SERVER[HTTP_ACCEPT_ENCODING]' ,
+  '$_SERVER[HTTP_ACCEPT_LANGUAGE]' ,'$_SERVER[HTTP_CONNECTION]' ,'$_SERVER[HTTP_HOST]','$_SERVER[HTTP_REFERER]' ,
+  '$_SERVER[HTTP_USER_AGENT]' ,'$_SERVER[HTTPS]' );";
+$result = mysqli_query($conn, $query);
+
+echo  "query sucess <br>";
+
+//$geo = freegeoip_locate($_SERVER['REMOTE_ADDR']);
+
+//echo "Country: " . $geo['country_name'];
+
 // Broken:
 /*$headers = apache_request_headers();
 
 foreach ($headers as $header => $value) {
     echo "$header: $value <br />\n";
 }*/
-
-
 
 // Broken:
 /*$headers = getallheaders();
